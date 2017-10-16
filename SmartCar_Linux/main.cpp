782b57c6ff8df29c8395c1533e19a2040bc35f53
uufree <369470777@qq.com>
2017-10-16 21:54:11 +0800

    fix something

--- SmartCar_Linux/main.cpp
+++ SmartCar_Linux/main.cpp
@@ -1,11 +1,23 @@
-#include "mainwindow.h"
-#include <QApplication>
+#include"mainwindow.h"
+#include<QApplication>
+#include<QCoreApplication>
+#include<QMutex>
+#include<QThread>
+#include<QTcpSocket>
+#include<QHostAddress>
+#include<QDataStream>
+#include<QByteArray>
+#include<iostream>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QCoreApplication a(argc, argv);
+    QTcpSocket* clientSocket = new QTcpSocket;
+    clientSocket->connectToHost(QHostAddress("192.168.1.105"),6666);
+    QDataStream out(clientSocket);
+    out.setVersion(QDataStream::Qt_5_7);
+
+    out << "hello,world!";
 
     return a.exec();
 }
--- SmartCar_Raspberry/main.cpp
+++ SmartCar_Raspberry/main.cpp
@@ -1,11 +1,36 @@
-#include "mainwindow.h"
-#include <QApplication>
+#include<QApplication>
+#include<QCoreApplication>
+#include<QTcpServer>
+#include<QTcpSocket>
+#include<QDataStream>
+
+class ClientSocket;
+
+class TcpServer : public QTcpServer
+{
+    Q_OBJECT
+public:
+    TcpServer(QObject* parent) : QTcpServer(parent)
+    {};
+
+private:
+    void incomingConnection();
+}
+
+class ClientSocket : public QTcpSocket
+{
+    Q_OBJECT
+public:
+    ClientSocket(QObject* parent) : QTcpSocket(parent);
+private slots:
+    void readInSocket();
+}
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QCoreApplication a(argc, argv);
+    QTcpServer server;
+
 
     return a.exec();
 }
